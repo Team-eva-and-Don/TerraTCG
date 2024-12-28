dfd39fb12c6bfab2d7ad2d08718901fe566697d1
= <=>
2024-12-28 11:19:14 -0600

    Remove debug code that made opponent's hand visible

--- Common/UI/GameFieldUI/OpponentHandElement.cs
+++ Common/UI/GameFieldUI/OpponentHandElement.cs
@@ -39,10 +39,10 @@ namespace TerraTCG.Common.UI.GameFieldUI
             }
             Vector2 currentPos = CardPosition0;
 
-            // var texture = TextureCache.Instance.CardBack;
+            var texture = TextureCache.Instance.CardBack;
             foreach (var card in gamePlayer.Hand.Cards)
             {
-                var texture = card.Texture;
+                //var texture = card.Texture;
                 spriteBatch.Draw(texture.Value, currentPos, texture.Value.Bounds, Color.White, 0, default, CARD_SCALE, SpriteEffects.None, 0f);
                 currentPos.X += texture.Width() * CARD_SCALE + CARD_MARGIN;
             }
