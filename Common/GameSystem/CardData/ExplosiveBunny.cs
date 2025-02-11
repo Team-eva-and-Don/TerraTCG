aee01a122b2baa3a8754ba9ca4f018c2f2dfae7a
= <=>
2025-02-10 22:27:18 -0600

    Add 'critters matter' cards

---
+++ Common/GameSystem/CardData/ExplosiveBunny.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Terraria;
+using Terraria.ID;
+using TerraTCG.Common.GameSystem.GameState;
+using TerraTCG.Common.GameSystem.GameState.Modifiers;
+
+namespace TerraTCG.Common.GameSystem.CardData
+{
+    internal class ExplosiveBunny : BaseCardTemplate, ICardTemplate
+    {
+
+		private class ExplosiveBunnySelfDamageModifier : ICardModifier
+		{
+			public void ModifyAttack(ref Attack attack, Zone sourceZone, Zone destZone) 
+			{
+				var critterCount = sourceZone.Siblings
+					.Where(z => z.PlacedCard is PlacedCard card && card.Template.SubTypes.Contains(CardSubtype.CRITTER))
+					.Count();
+
+				attack.SelfDamage = Math.Max(0, attack.SelfDamage - critterCount);
+			}
+		}
+
+        public override Card CreateCard() => new ()
+        {
+            Name = "ExplosiveBunny",
+            MaxHealth = 8,
+            CardType = CardType.CREATURE,
+            NPCID = NPCID.ExplosiveBunny,
+            SubTypes = [CardSubtype.FOREST, CardSubtype.SCOUT],
+            Attacks = [
+                new() {
+                    Damage = 4,
+                    Cost = 2,
+                    SelfDamage = 3,
+                }
+            ],
+			Modifiers = () => [
+				new ExplosiveBunnySelfDamageModifier()
+			],
+        };
+    }
+}
---
+++ Common/GameSystem/CardData/GuideToCritters.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TerraTCG.Common.GameSystem.Drawing;
+using TerraTCG.Common.GameSystem.GameState;
+using TerraTCG.Common.GameSystem.GameState.GameActions;
+using TerraTCG.Common.GameSystem.GameState.Modifiers;
+
+namespace TerraTCG.Common.GameSystem.CardData
+{
+    internal class GuideToCritters : BaseCardTemplate, ICardTemplate
+    {
+		private class GuideToCrittersTargetModifier : ICardModifier
+		{
+			public Asset<Texture2D> Texture { get; set; }
+
+			public Card SourceCard { get; set; }
+
+            public void ModifyIncomingZoneSelection(Zone sourceZone, Zone endZone, ref List<Zone> destZones)
+            {
+                // Only allow attacks against the turtle
+                if(!sourceZone.IsEmpty())
+                {
+					destZones = destZones.Where(z => z.PlacedCard is PlacedCard card && !card.Template.SubTypes.Contains(CardSubtype.CRITTER)).ToList();
+					if(destZones.Count == 0)
+					{
+						destZones = [sourceZone];
+					}
+                }
+            }
+
+		}
+
+        public override Card CreateCard() => new ()
+        {
+            Name = "GuideToCritters",
+            CardType = CardType.ITEM,
+            SubTypes = [CardSubtype.EQUIPMENT, CardSubtype.ITEM],
+            SelectInHandAction = (card, player) => new ApplyModifierAction(card, player),
+            ShouldTarget = z => true,
+            Skills = [ 
+                new() { Cost = 2 }
+            ],
+            Modifiers = () => [
+                new GuideToCrittersTargetModifier()  {
+                    Texture = TextureCache.Instance.GetItemTexture(ItemID.DontHurtCrittersBook),
+                }
+            ]
+        };
+    }
+}
