6f4143c2ec6d04009290c1586d5f15f3e0731edc
かつ丼さん <109509123+Katudonsan@users.noreply.github.com>
2025-06-18 20:08:14 +0900

    Update Skill.cs

--- Common/GameSystem/GameState/Skill.cs
+++ Common/GameSystem/GameState/Skill.cs
@@ -9,19 +9,19 @@ using TerraTCG.Common.GameSystem.GameState.GameActions;
 
 namespace TerraTCG.Common.GameSystem.GameState
 {
-    internal delegate void DoSkill(GamePlayer player, Zone cardZone, Zone endZone);
-    internal struct Skill()
+	public delegate void DoSkill(GamePlayer player, Zone cardZone, Zone endZone);
+	public struct Skill()
     {
         public Asset<Texture2D> Texture { get; internal set; }
-        internal string Name { get; set; }
-        internal int Cost { get; set; }
-        internal string Description { get; set; }
+		public string Name { get; set; }
+		public int Cost { get; set; }
+		public string Description { get; set; }
 
         // Flag used by bots to determine how to select targets for skills
-        internal ZoneRole Role { get; set; } = ZoneRole.OFFENSE;
+		public ZoneRole Role { get; set; } = ZoneRole.OFFENSE;
 
-        internal ActionType SkillType { get; set; } = ActionType.SKILL;
-        internal DoSkill DoSkill { get; set; }
+		public ActionType SkillType { get; set; } = ActionType.SKILL;
+		public DoSkill DoSkill { get; set; }
 
         public Skill Copy()
         {
