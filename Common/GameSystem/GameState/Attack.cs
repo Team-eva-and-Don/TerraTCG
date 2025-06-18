0106a8c63de4c565d68daf165af9d3f0ccf4d022
eva828 <88603178+eva828@users.noreply.github.com>
2025-06-18 19:53:04 +0900

    Update Attack.cs

--- Common/GameSystem/GameState/Attack.cs
+++ Common/GameSystem/GameState/Attack.cs
@@ -7,24 +7,24 @@ using TerraTCG.Common.GameSystem.GameState.Modifiers;
 
 namespace TerraTCG.Common.GameSystem.GameState
 {
-    internal delegate void DoAttack(Attack attack, Zone sourceZone, Zone targetZone);
+    public delegate void DoAttack(Attack attack, Zone sourceZone, Zone targetZone);
 
-    internal delegate List<ICardModifier> ApplyModifiers(Zone appliedZone);
-    internal struct Attack()
+    public delegate List<ICardModifier> ApplyModifiers(Zone appliedZone);
+    public struct Attack()
     {
-        internal string Name { get; set; }
-        internal string Description { get; set; }
-        internal int Damage { get; set; }
-        internal int SelfDamage { get; set; }
-        internal int Cost { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Damage { get; set; }
+        public int SelfDamage { get; set; }
+        public int Cost { get; set; }
 
-        internal DoAttack DoAttack { get; set; } = DefaultAttack;
+        public DoAttack DoAttack { get; set; } = DefaultAttack;
 
         // Modifiers applied to the attacker after the attack goes through
-        internal ApplyModifiers SourceModifiers { get; set; }
+        public ApplyModifiers SourceModifiers { get; set; }
 
         // Modifiers applied to the target after the attack goes through
-        internal ApplyModifiers TargetModifiers { get; set; }
+        public ApplyModifiers TargetModifiers { get; set; }
 
 
         // How much damage this attack does per mana spent, used by bots
