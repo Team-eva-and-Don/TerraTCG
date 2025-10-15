7db30ba0d8dcfae78fab06783fe7d4c965d88121
かつ丼さん <109509123+Katudonsan@users.noreply.github.com>
2025-10-15 18:49:55 +0900

    Revert Card Edit.

--- Common/GameSystem/GameState/Card.cs
+++ Common/GameSystem/GameState/Card.cs
@@ -1,55 +1,60 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent;
 using Terraria.Localization;
 using Terraria.ModLoader;
 using TerraTCG.Common.GameSystem.Drawing;
 using TerraTCG.Common.GameSystem.GameState.GameActions;
 using TerraTCG.Common.GameSystem.GameState.Modifiers;
+using TerraTCG.Common.Util;
 
 namespace TerraTCG.Common.GameSystem.GameState
 {
 	public enum CardType
-	{
-		CREATURE,
-		ITEM,
-		TOWNSFOLK
-	}
+    {
+        CREATURE,
+        ITEM,
+        TOWNSFOLK
+    }
 
 	public enum CardSubtype
-	{
-		// Empty value
-		NONE,
-		// Creature Supertypes
-		EXPERT,
-		BOSS,
-		// Creature Roles
-		FIGHTER,
-		SCOUT,
-		DEFENDER,
-		SLIME,
-		CRITTER,
-		CASTER,
+    {
+        // Empty value
+        NONE,
+        // Creature Supertypes
+        EXPERT,
+        BOSS,
+        // Creature Roles
+        FIGHTER,
+        SCOUT,
+        DEFENDER,
+        SLIME,
+        CRITTER,
+        CASTER,
 
-		// Biomes
-		FOREST,
-		CAVERN,
-		JUNGLE,
-		BLOOD_MOON,
-		OCEAN,
-		GOBLIN_ARMY,
-		MUSHROOM,
+        // Biomes
+        FOREST,
+        CAVERN,
+        JUNGLE,
+        BLOOD_MOON,
+        OCEAN,
+        GOBLIN_ARMY,
+        MUSHROOM,
 
-		// Item Subtypes
-		EQUIPMENT,
-		CONSUMABLE,
+        // Item Subtypes
+        EQUIPMENT,
+        CONSUMABLE,
 
-		// Duplicate CardTypes (for text rendering)
-		TOWNSFOLK,
-		ITEM,
+        // Duplicate CardTypes (for text rendering)
+        TOWNSFOLK,
+        ITEM,
 		EVIL,
 		OWNED,
 		SNOW,
@@ -57,9 +62,9 @@ namespace TerraTCG.Common.GameSystem.GameState
 		DESERT,
 	}
 
-	// Bot helper function, apply additional
-	// logic for whether the bot should use
-	// this item on an ally
+    // Bot helper function, apply additional
+    // logic for whether the bot should use
+    // this item on an ally
 	public delegate bool ShouldTarget(Zone zone);
 
 	// Delegate function type to check whether a given card can
@@ -67,13 +72,13 @@ namespace TerraTCG.Common.GameSystem.GameState
 	public delegate bool CanPromote(Zone zone, Card card);
 
 	public class Card
-	{
+    {
 		public string Name { get; set; }
 
-		// The mod that originated this card
+        // The mod that originated this card
 		public string Mod { get; set; } = ModContent.GetInstance<TerraTCG>().Name;
 
-		internal string FullName => $"{Mod}/{Name}";
+        internal string FullName => $"{Mod}/{Name}";
 
 		// Automatically loaded from its texture
 		public int GoreType { get; set; }
@@ -94,8 +99,8 @@ namespace TerraTCG.Common.GameSystem.GameState
 
 		public List<CardSubtype> SubTypes { get; set; }
 
-		internal CardSubtype SortType =>
-			(SubTypes[0] == CardSubtype.EXPERT || SubTypes[0] == CardSubtype.BOSS) ? SubTypes[1] : SubTypes[0];
+        internal CardSubtype SortType => 
+            (SubTypes[0] == CardSubtype.EXPERT || SubTypes[0] == CardSubtype.BOSS) ? SubTypes[1] : SubTypes[0];
 
 		internal CardSubtype RoleSubtype => SubTypes.Last();
 
@@ -103,20 +108,20 @@ namespace TerraTCG.Common.GameSystem.GameState
 
 		public int MoveCost { get; set; }
 
-		// Points awarded on defeating this card
+        // Points awarded on defeating this card
 		public int Points { get; set; } = 1;
 
-		// Used for cards that can only be created by other cards (eg. Nymph)
+        // Used for cards that can only be created by other cards (eg. Nymph)
 		public bool IsCollectable { get; set; } = true;
 
 		public SelectInHandAction SelectInHandAction { get; set; }
-			= (zone, player) => new DeployCreatureAction(zone, player);
+            = (zone, player) => new DeployCreatureAction(zone, player);
 		public SelectOnFieldAction SelectOnFieldAction { get; set; }
-			= (zone, player) => new MoveCardOrAttackAction(zone, player);
+            = (zone, player) => new MoveCardOrAttackAction(zone, player);
 
 		public List<Skill> Skills { get; set; }
 
-		internal bool HasSkill => (Skills?.Count ?? 0) > 0;
+        internal bool HasSkill => (Skills?.Count ?? 0) > 0;
 
 		public List<Attack> Attacks { get; set; }
 
@@ -130,13 +135,13 @@ namespace TerraTCG.Common.GameSystem.GameState
 		// ShouldRemove(GameEvent.START_TURN)
 		public Func<List<ICardModifier>> FieldModifiers { get; set; }
 
-		// Tags to help bot players decide what to do with cards
+        // Tags to help bot players decide what to do with cards
 
-		// Where to place the card on the board
+        // Where to place the card on the board
 		public ZoneRole Role { get; set; } = ZoneRole.OFFENSE;
 
 
-		// Among cards of the same type, how important is it to play this one first?
+        // Among cards of the same type, how important is it to play this one first?
 		public int Priority { get; set; } = 0;
 
 		// Helper func for the bot player that suggests whether a zone would be a good target
@@ -148,63 +153,63 @@ namespace TerraTCG.Common.GameSystem.GameState
 
 		// Func to check whether this card can evolve onto a given card on the field
 		// By default, check whether the biome and combat role match
-		public CanPromote CanPromote { get; set; } = (zone, card) =>
+		public CanPromote CanPromote { get; set; } = (zone, card) => 
 			zone.PlacedCard?.Template.SubTypes[0] == card.SubTypes[1] &&
 			zone.PlacedCard?.Template.SubTypes.Last() == card.SubTypes.Last();
 
 
-		// Localization utils
-		internal string CardName => Language.GetTextValue($"Mods.{Mod}.Cards.{Name}.Name");
+        // Localization utils
+        internal string CardName => Language.GetTextValue($"Mods.{Mod}.Cards.{Name}.Name");
 
-		internal string AttackName => Language.GetTextValue($"Mods.{Mod}.Cards.{Name}.Attack.Name");
-		internal string AttackDescription => Language.GetTextValue($"Mods.{Mod}.Cards.{Name}.Attack.Description");
-		internal string SkillName => Language.GetTextValue($"Mods.{Mod}.Cards.{Name}.Skill.Name");
-		internal string SkillDescription => Language.GetTextValue($"Mods.{Mod}.Cards.{Name}.Skill.Description");
+        internal string AttackName => Language.GetTextValue($"Mods.{Mod}.Cards.{Name}.Attack.Name");
+        internal string AttackDescription => Language.GetTextValue($"Mods.{Mod}.Cards.{Name}.Attack.Description");
+        internal string SkillName => Language.GetTextValue($"Mods.{Mod}.Cards.{Name}.Skill.Name");
+        internal string SkillDescription => Language.GetTextValue($"Mods.{Mod}.Cards.{Name}.Skill.Description");
 
-		internal string ModifierDescription => Language.GetTextValue($"Mods.{Mod}.Cards.{Name}.Modifier.Description");
+        internal string ModifierDescription => Language.GetTextValue($"Mods.{Mod}.Cards.{Name}.Modifier.Description");
 
-		internal bool HasAttackText => Language.Exists($"Mods.{Mod}.Cards.{Name}.Attack.Name");
-		internal bool HasAttackDescription => HasAttackText && Language.Exists($"Mods.{Mod}.Cards.{Name}.Attack.Description");
-		internal bool HasSkillText => Language.Exists($"Mods.{Mod}.Cards.{Name}.Skill.Name");
-		internal bool HasSkillDescription => HasSkillText && Language.Exists($"Mods.{Mod}.Cards.{Name}.Skill.Description");
-		internal bool HasModifierText => Language.Exists($"Mods.{Mod}.Cards.{Name}.Modifier.Description");
+        internal bool HasAttackText => Language.Exists($"Mods.{Mod}.Cards.{Name}.Attack.Name");
+        internal bool HasAttackDescription => HasAttackText && Language.Exists($"Mods.{Mod}.Cards.{Name}.Attack.Description");
+        internal bool HasSkillText => Language.Exists($"Mods.{Mod}.Cards.{Name}.Skill.Name");
+        internal bool HasSkillDescription => HasSkillText && Language.Exists($"Mods.{Mod}.Cards.{Name}.Skill.Description");
+        internal bool HasModifierText => Language.Exists($"Mods.{Mod}.Cards.{Name}.Modifier.Description");
 
 		internal string TypeLine => string.Join(" ",
 			SubTypes.Select(t => Language.Exists($"Mods.{Mod}.Cards.Types.{t}") ? Language.GetTextValue($"Mods.{Mod}.Cards.Types.{t}") : Language.GetTextValue($"Mods.TerraTCG.Cards.Types.{t}")));
 
-		public DrawZoneNPC DrawZoneNPC { get; set; } = CardOverlayRenderer.Instance.DefaultDrawZoneNPC;
+        public DrawZoneNPC DrawZoneNPC { get; set; } = CardOverlayRenderer.Instance.DefaultDrawZoneNPC;
 
 		// Do a search of whether any text on the card contains the search term
 		internal bool MatchesTextFilter(string textFilter)
-		{
-			var allMyText = new StringBuilder();
-			// All cards have a name and typeline
-			allMyText.Append(CardName);
-			allMyText.Append(TypeLine);
+        {
+            var allMyText = new StringBuilder();
+            // All cards have a name and typeline
+            allMyText.Append(CardName);
+            allMyText.Append(TypeLine);
 
-			// Cards variably have attacks, skills, and static modifiers
-			if (HasAttackText)
-			{
-				allMyText.Append(AttackName);
-			}
-			if (HasAttackDescription)
-			{
-				allMyText.Append(AttackDescription);
-			}
-			if (HasSkill)
-			{
-				allMyText.Append(SkillName);
-			}
-			if (HasSkillDescription)
-			{
-				allMyText.Append(SkillDescription);
-			}
-			if (HasModifierText)
-			{
-				allMyText.Append(ModifierDescription);
-			}
+            // Cards variably have attacks, skills, and static modifiers
+            if(HasAttackText)
+            {
+                allMyText.Append(AttackName);
+            }
+            if(HasAttackDescription)
+            {
+                allMyText.Append(AttackDescription);
+            }
+            if(HasSkill)
+            {
+                allMyText.Append(SkillName);
+            }
+            if (HasSkillDescription)
+            {
+                allMyText.Append(SkillDescription);
+            }
+            if(HasModifierText)
+            {
+                allMyText.Append(ModifierDescription);
+            }
 
-			return allMyText.ToString().Replace("\n", " ").Contains(textFilter, StringComparison.CurrentCultureIgnoreCase);
-		}
-	}
+            return allMyText.ToString().Replace("\n"," ").Contains(textFilter, StringComparison.CurrentCultureIgnoreCase);
+        }
+    }
 }
